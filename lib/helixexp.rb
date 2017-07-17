d5ca697a6590a3416d4cef99f471f18338e1ab97
SAWADA Tadashi <cesare@mayverse.jp>
2017-07-17 14:36:19 +0900

    Fix the top-level module into a class

--- lib/helixexp.rb
+++ lib/helixexp.rb
@@ -1,5 +1,3 @@
 require "helixexp/version"
-
-module Helixexp
-  # Your code goes here...
-end
+require 'helix_runtime'
+require 'helixexp/native'
--- lib/helixexp/version.rb
+++ lib/helixexp/version.rb
@@ -1,3 +1,3 @@
-module Helixexp
+class Helixexp
   VERSION = "0.1.0"
 end
